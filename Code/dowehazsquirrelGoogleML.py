a9ad5e60e931d36d0ddd02c42b20b1f5e173de98
macgyver <timothy_meyer@hotmail.com>
2018-11-03 11:18:03 -0400

    asdf

--- Code/dowehazsquirrelGoogleML.py
+++ Code/dowehazsquirrelGoogleML.py
@@ -7,20 +7,18 @@ from google.cloud import vision
 from google.cloud.vision import types
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/pi/squirrelcred.json"
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient()
-# The name of the image file to annotate
-# file_name = os.path.join(
-#     os.path.dirname(__file__),
-#     'resources/wakeupcat.jpg')
 
-# Loads the image into memory
-# with io.open(file_name, 'rb') as image_file:
-#     content = image_file.read()
-# image = types.Image(content=content)
 
-def SpotObject(image, tag, confidence):
-    # Performs label detection on the image file
+def SpotObject(content, tag, confidence):
+    # Performs label detection on the content file (io.ByteIO), tag to search for, confidence required for match
+
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # copies the image into memory
+    #content = image.read()
+
+    image = vision.types.Image(content=content)
     response = client.label_detection(image=image)
     labels = response.label_annotations
 
@@ -29,4 +27,4 @@ def SpotObject(image, tag, confidence):
         #if((label.description == tag) and (label.score > confidence):
         print(label.description, label.score)
         #return(true)
-    return(False)
+    return(False)# Instantiates a client
