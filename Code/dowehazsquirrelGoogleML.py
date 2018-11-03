b3706b4b537fb8d7ee1ea5a991143a99689e55a8
macgyver <timothy_meyer@hotmail.com>
2018-11-03 09:24:20 -0400

    asdf

--- Code/dowehazsquirrelGoogleML.py
+++ Code/dowehazsquirrelGoogleML.py
@@ -19,7 +19,7 @@ client = vision.ImageAnnotatorClient()
 # with io.open(file_name, 'rb') as image_file:
 #     content = image_file.read()
 # image = types.Image(content=content)
-def SpotObject (image, tag, confidence):
+def SpotObject(image, tag, confidence):
     # Performs label detection on the image file
     response = client.label_detection(image=image)
     labels = response.label_annotations
