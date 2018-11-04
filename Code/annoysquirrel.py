b75fdc219632c762c06e205bfe8ab9d5c9054a31
macgyver <timothy_meyer@hotmail.com>
2018-11-04 10:51:15 -0500

    derp, copypasta

--- Code/annoysquirrel.py
+++ Code/annoysquirrel.py
@@ -1,12 +1,14 @@
 import requests
 import io
 
+# take the credential key that you got from your webhook in IFTTT and put it in this file as a single line of text
 IFTTT_APPLICATION_FILE = "/home/pi/iftttcreds.txt"
+
 with open (IFTTT_APPLICATION_FILE, "r") as credfile:
     IFTTT_APPLICATION_CREDENTIALS=credfile.readlines()
 
 def annoySquirrelOn():
-        requests.get('https://maker.ifttt.com/trigger/squirrelOn/with/key/IFTTT_APPLICATION_CREDENTIALS')
+        requests.get('https://maker.ifttt.com/trigger/squirrelOn/with/key/',IFTTT_APPLICATION_CREDENTIALS)
 
 def annoySquirrelOff():
-        requests.get('https://maker.ifttt.com/trigger/squirrelOff/with/key/IFTTT_APPLICATION_CREDENTIALS')
+        requests.get('https://maker.ifttt.com/trigger/squirrelOff/with/key/',IFTTT_APPLICATION_CREDENTIALS)
